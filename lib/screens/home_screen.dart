2bde2994e4922fdfd1d0b343d4d81047e649bda3
MHabib96 <33306932+MHabib96@users.noreply.github.com>
2021-08-03 11:40:28 +0100

    Update the home screen
     - Add highlight slider
     - Add category showcase
     - Add movie list views
     - Add bottom navigation bar

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,25 +1,74 @@
 import 'package:disney_plus/components/builders/responsive_base.dart';
 import 'package:disney_plus/components/category_showcase.dart';
+import 'package:disney_plus/components/custom_bottom_navigation_bar.dart';
+import 'package:disney_plus/components/highlight_slider.dart';
+import 'package:disney_plus/components/movie_list_view.dart';
 import 'package:disney_plus/utilities/constants.dart';
 import 'package:flutter/material.dart';
 
+const String movieBase = 'assets/images/movies/';
+
 class HomeScreen extends StatelessWidget {
+  List<String> _newToDisneyPlus() {
+    return [
+      "${movieBase}chip_n_dale.PNG",
+      "${movieBase}black_widow.PNG",
+      "${movieBase}turning_the_tables.PNG",
+      "${movieBase}summer_of_soul.PNG",
+      "${movieBase}high_school_musical.PNG",
+      "${movieBase}mickey_mouse.PNG",
+    ];
+  }
+
+  List<String> _recommended() {
+    return [
+      "${movieBase}avengers_aou.PNG",
+      "${movieBase}runaways.PNG",
+      "${movieBase}hamilton.PNG",
+      "${movieBase}girl_vs_monster.PNG",
+      "${movieBase}blackish.PNG",
+      "${movieBase}boy_meets_world.PNG",
+    ];
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Color.fromRGBO(19, 21, 32, 1),
       body: ResponsiveBase(
         builder: (context, details) {
-          return Column(
+          return ListView(
+            physics: BouncingScrollPhysics(),
             children: [
-              Image.asset(kHomeScreenLogo),
+              SizedBox(height: 10),
+              SizedBox(
+                height: 50,
+                child: Image.asset(kHomeScreenLogo),
+              ),
+              SizedBox(height: 5),
+              HighlightSlider(),
+              SizedBox(height: 20),
               Padding(
                 padding: const EdgeInsets.symmetric(horizontal: 20),
                 child: CategoryShowcase(),
               ),
+              SizedBox(height: 20),
+              MovieListView(
+                label: 'New to Disney+',
+                images: _newToDisneyPlus(),
+              ),
+              SizedBox(height: 20),
+              MovieListView(
+                label: 'Recommended For You',
+                images: _recommended(),
+              ),
             ],
           );
         },
+      ),
+      bottomNavigationBar: CustomBottomNavigationBar(
+        currentIndex: 0,
+        onTap: (int) => print('tapped'),
       ),
     );
   }
